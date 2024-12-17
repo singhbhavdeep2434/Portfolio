7d157e215ee78cc2e76b9e4e14999319cb72fcff
singhbhavdeep2434 <singh.bhavdeep2434@gmail.com>
2024-12-16 19:31:19 -0500

    Initial commit

---
+++ src/data/certifications.ts
@@ -0,0 +1,14 @@
+export const certificationsData = [
+  {
+    title: "Java Spring Framework 6 with Spring Boot 3",
+    issuer: "Udemy",
+    date: "2024",
+    credentialUrl: ""
+  },
+  {
+    title: "Node.js",
+    issuer: "Net Ninja",
+    date: "2024",
+    credentialUrl: ""
+  }
+];
